ff1b659dbde49cdd4778f4cfa08c50c3c3132d9e
edu-de <66421684+edu-de@users.noreply.github.com>
2021-06-15 11:04:14 +0200

    some refactoring

--- src/net/datenwerke/rs/fileserver/service/fileserver/terminal/commands/WebAccessSubCommand.java
+++ src/net/datenwerke/rs/fileserver/service/fileserver/terminal/commands/WebAccessSubCommand.java
@@ -8,6 +8,10 @@ import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
 
+import net.datenwerke.gf.service.history.HistoryService;
+import net.datenwerke.rs.fileserver.service.fileserver.FileServerService;
+import net.datenwerke.rs.fileserver.service.fileserver.entities.FileServerFolder;
+import net.datenwerke.rs.fileserver.service.fileserver.locale.FileserverMessages;
 import net.datenwerke.rs.terminal.service.terminal.TerminalSession;
 import net.datenwerke.rs.terminal.service.terminal.helpers.AutocompleteHelper;
 import net.datenwerke.rs.terminal.service.terminal.helpers.CommandParser;
@@ -16,82 +20,82 @@ import net.datenwerke.rs.terminal.service.terminal.helpmessenger.annotations.Non
 import net.datenwerke.rs.terminal.service.terminal.obj.CommandResult;
 import net.datenwerke.rs.terminal.service.terminal.objresolver.exceptions.ObjectResolverException;
 import net.datenwerke.security.service.security.rights.Read;
-import net.datenwerke.gf.service.history.HistoryLink;
-import net.datenwerke.gf.service.history.HistoryService;
-import net.datenwerke.rs.fileserver.service.fileserver.FileServerService;
-import net.datenwerke.rs.fileserver.service.fileserver.entities.FileServerFolder;
-import net.datenwerke.rs.fileserver.service.fileserver.locale.FileserverMessages;
 
 public class WebAccessSubCommand implements DirModCommandHook {
-	private final Logger logger = LoggerFactory.getLogger(getClass().getName());
-	public static final String BASE_COMMAND = "webaccess";
+   private final Logger logger = LoggerFactory.getLogger(getClass().getName());
+   public static final String BASE_COMMAND = "webaccess";
 
-	private final FileServerService fileServerService;
-	private final HistoryService historyService;
+   private final FileServerService fileServerService;
+   private final HistoryService historyService;
 
-	@Inject
-	public WebAccessSubCommand(FileServerService fileServerService, HistoryService historyService) {
-		/* store objects */
-		this.fileServerService = fileServerService;
-		this.historyService = historyService;
-	}
+   @Inject
+   public WebAccessSubCommand(FileServerService fileServerService, HistoryService historyService) {
+      /* store objects */
+      this.fileServerService = fileServerService;
+      this.historyService = historyService;
+   }
 
-	@Override
-	public boolean consumes(CommandParser parser, TerminalSession session) {
-		return BASE_COMMAND.equals(parser.getBaseCommand());
-	}
+   @Override
+   public boolean consumes(CommandParser parser, TerminalSession session) {
+      return BASE_COMMAND.equals(parser.getBaseCommand());
+   }
 
-	@CliHelpMessage(messageClass = FileserverMessages.class, name = BASE_COMMAND, description = "commandWebAccess_description", nonOptArgs = {
-			@NonOptArgument(name = "directory", description = "commandDirMod_description_arg_dirpath", mandatory = true),
-			@NonOptArgument(name = "access", description = "commandDirMod_description_arg_webaccess", mandatory = true) })
-	@Override
-	public CommandResult execute(CommandParser parser, TerminalSession session) throws ObjectResolverException {
-		CommandResult cr = new CommandResult();
-		List<String> arguments = parser.getNonOptionArguments();
-		if (arguments.isEmpty() || arguments.size() != 2)
-			throw new IllegalArgumentException("Please enter valid number of arguments");
+   @CliHelpMessage(
+         messageClass = FileserverMessages.class, 
+         name = BASE_COMMAND, 
+         description = "commandWebAccess_description", 
+         nonOptArgs = {
+               @NonOptArgument(name = "directory", description = "commandDirMod_description_arg_dirpath", mandatory = true),
+               @NonOptArgument(name = "access", description = "commandDirMod_description_arg_webaccess", mandatory = true) 
+               })
+   @Override
+   public CommandResult execute(CommandParser parser, TerminalSession session) throws ObjectResolverException {
+      final CommandResult cr = new CommandResult();
+      List<String> arguments = parser.getNonOptionArguments();
+      if (arguments.isEmpty() || arguments.size() != 2)
+         throw new IllegalArgumentException("Please enter valid number of arguments");
 
-		String path = arguments.get(0);
-		String webAccess = arguments.get(1);
+      String path = arguments.get(0);
+      final String webAccess = arguments.get(1);
 
-		if (!"true".equals(webAccess) && !"false".equals(webAccess))
-			throw new IllegalArgumentException("Please add valid webaccess value (true / false)");
+      if (!"true".equals(webAccess) && !"false".equals(webAccess))
+         throw new IllegalArgumentException("Please add valid webaccess value (true / false)");
 
-		Collection<Object> resolvedObjects = session.getObjectResolver().getObjects(path, Read.class);
-		List<?> searchResults = (List<?>) resolvedObjects;
+      final Collection<?> resolvedObjects = session.getObjectResolver().getObjects(path, Read.class);
+      final List<?> searchResults = (List<?>) resolvedObjects;
+      
+      if (0 == resolvedObjects.size()) {
+         cr.addResultLine("No FileServerFolder objects found");
+         return cr;
+      }
 
-		if (0 != resolvedObjects.size()) {
-			for (Object item : searchResults) {
-				if (!(item instanceof FileServerFolder))
-					throw new IllegalArgumentException("Only FileServerFolder objects can be modified");
-			}
-			cr.addResultLine("Web access permission changed for the following directories:");
-			for (Object item : searchResults) {
-				FileServerFolder folder = (FileServerFolder) item;
-				folder.setPubliclyAccessible(Boolean.parseBoolean(webAccess));
-				fileServerService.persist(folder);
-				try {
-					List<HistoryLink> links = historyService.buildLinksFor(item);
-					for (HistoryLink link : links) {
-						cr.addResultHyperLink(link.getObjectCaption(), link.getLink());
-					}
-				} catch (IllegalArgumentException e) {
-					logger.warn(e.getMessage(), e);
-				}
-			}
-		} else {
-			cr.addResultLine("No FileServerFolder objects found");
-		}
-		return cr;
-	}
+      if (searchResults
+            .stream()
+            .anyMatch(item -> !(item instanceof FileServerFolder)))
+         throw new IllegalArgumentException("Only FileServerFolder objects can be modified");
+         
+      cr.addResultLine("Web access permission changed for the following directories:");
+      searchResults.forEach(item -> {
+         FileServerFolder folder = (FileServerFolder) item;
+         folder.setPubliclyAccessible(Boolean.parseBoolean(webAccess));
+         fileServerService.persist(folder);
+         try {
+            historyService.buildLinksFor(item)
+               .forEach(link -> cr.addResultHyperLink(link.getObjectCaption(), link.getLink()));
+         } catch (IllegalArgumentException e) {
+            logger.warn(e.getMessage(), e);
+         }
+      });
+      return cr;
+   }
 
-	@Override
-	public void addAutoCompletEntries(AutocompleteHelper autocompleteHelper, TerminalSession session) {
-		autocompleteHelper.autocompleteBaseCommand(BASE_COMMAND);
-	}
+   @Override
+   public void addAutoCompletEntries(AutocompleteHelper autocompleteHelper, TerminalSession session) {
+      autocompleteHelper.autocompleteBaseCommand(BASE_COMMAND);
+   }
 
-	@Override
-	public String getBaseCommand() {
-		return BASE_COMMAND;
-	}
+   @Override
+   public String getBaseCommand() {
+      return BASE_COMMAND;
+   }
 }
