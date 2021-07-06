a58cf90ebf582f6415478eb39fbda919e8887084
edu-de <66421684+edu-de@users.noreply.github.com>
2021-07-06 09:30:43 +0200

    Format

--- src/net/datenwerke/rs/adminutils/server/datasourcetester/DatasourceTesterRPCServiceImpl.java
+++ src/net/datenwerke/rs/adminutils/server/datasourcetester/DatasourceTesterRPCServiceImpl.java
@@ -30,85 +30,85 @@ import net.datenwerke.security.service.security.rights.Execute;
 import net.datenwerke.security.service.security.rights.Read;
 
 @Singleton
-public class DatasourceTesterRPCServiceImpl extends SecuredRemoteServiceServlet
-		implements DatasourceTesterRPCService {
+public class DatasourceTesterRPCServiceImpl extends SecuredRemoteServiceServlet implements DatasourceTesterRPCService {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3945467345321105023L;
+   /**
+    * 
+    */
+   private static final long serialVersionUID = 3945467345321105023L;
 
-	private final DtoService dtoService;
-	private final DBHelperService dbHelperService;
-	private final SimpleDataSupplier simpleDataSupplyer;
-	private final ExceptionServices exceptionServices;
-	private final Provider<HookHandlerService> hookHandlerServiceProvider;
-	
-	@Inject
-	public DatasourceTesterRPCServiceImpl(
-		DtoService dtoService,
-		DBHelperService dbHelperService,
-		SimpleDataSupplier simpleDataSupplyer,
-		ExceptionServices exceptionServices,
-		Provider<HookHandlerService> hookHandlerServiceProvider
-		){
-		
-		/* store objects */
-		this.dtoService = dtoService;
-		this.dbHelperService = dbHelperService;
-		this.simpleDataSupplyer = simpleDataSupplyer;
-		this.exceptionServices = exceptionServices;
-		this.hookHandlerServiceProvider = hookHandlerServiceProvider;
-	}
-	
-	@SecurityChecked(
-		argumentVerification = {
-			@ArgumentVerification(
-				name = "datasource",
-				isDto = true,
-				verify = @RightsVerification(rights={Read.class, Execute.class})
-			)
-		}
-	)
-	@Transactional(rollbackOn={Exception.class})
-	@Override
-	public boolean testConnection(@Named("datasource")DatabaseDatasourceDto databaseDto) throws ConnectionTestFailedException {
-		/* create real datasource */
-		DatabaseDatasource datasource = (DatabaseDatasource) dtoService.loadPoso(databaseDto);
-		
-		/* get db helper */
-		DatabaseHelper dbHelper = dbHelperService.getDatabaseHelper(datasource.getDatabaseDescriptor());
-		if(null == dbHelper)
-			throw new ConnectionTestFailedException("No database type specified"); //$NON-NLS-1$
-		
-		/* check if anybody wants to take over */
-		Optional<DatabaseConnectionTesterHook> takesOver = 
-				hookHandlerServiceProvider.get().getHookers(DatabaseConnectionTesterHook.class)
-				.stream()
-				.filter(tester -> tester.consumes(datasource, dbHelper))
-				.findAny();
-		
-		if (takesOver.isPresent())
-			return takesOver.get().testConnection(datasource, dbHelper);
-		
-		/* create config */
-		DatabaseDatasourceConfig config = (DatabaseDatasourceConfig) datasource.createConfigObject();
-		config.setQuery(dbHelper.createDummyQuery());
-		
-		/* build dummy table report */
-		TableReport report = new TableReport();
-		DatasourceContainer datasourceContainer = new DatasourceContainer(datasource, config);
-		report.setDatasourceContainer(datasourceContainer);
-		
-		try{
-			simpleDataSupplyer.getData(report);
-		} catch(Exception e){
-			ConnectionTestFailedException ex = new ConnectionTestFailedException(e.getMessage(),e);
-			ex.setStackTraceAsString(exceptionServices.exceptionToString(e));
-			throw ex;
-		}
-		
-		return true;
-	}
+   private final DtoService dtoService;
+   private final DBHelperService dbHelperService;
+   private final SimpleDataSupplier simpleDataSupplyer;
+   private final ExceptionServices exceptionServices;
+   private final Provider<HookHandlerService> hookHandlerServiceProvider;
+
+   @Inject
+   public DatasourceTesterRPCServiceImpl(
+         DtoService dtoService, 
+         DBHelperService dbHelperService,
+         SimpleDataSupplier simpleDataSupplyer, 
+         ExceptionServices exceptionServices,
+         Provider<HookHandlerService> hookHandlerServiceProvider
+         ) {
+
+      /* store objects */
+      this.dtoService = dtoService;
+      this.dbHelperService = dbHelperService;
+      this.simpleDataSupplyer = simpleDataSupplyer;
+      this.exceptionServices = exceptionServices;
+      this.hookHandlerServiceProvider = hookHandlerServiceProvider;
+   }
+
+   @SecurityChecked(argumentVerification = {
+         @ArgumentVerification(
+               name = "datasource", 
+               isDto = true, 
+               verify = @RightsVerification(
+                     rights = { 
+                           Read.class,
+                           Execute.class 
+                           })) })
+   @Transactional(rollbackOn = { Exception.class })
+   @Override
+   public boolean testConnection(@Named("datasource") DatabaseDatasourceDto databaseDto)
+         throws ConnectionTestFailedException {
+      /* create real datasource */
+      final DatabaseDatasource datasource = (DatabaseDatasource) dtoService.loadPoso(databaseDto);
+
+      /* get db helper */
+      final DatabaseHelper dbHelper = dbHelperService.getDatabaseHelper(datasource.getDatabaseDescriptor());
+      if (null == dbHelper)
+         throw new ConnectionTestFailedException("No database type specified"); //$NON-NLS-1$
+
+      /* check if anybody wants to take over */
+      Optional<DatabaseConnectionTesterHook> takesOver = hookHandlerServiceProvider.get()
+            .getHookers(DatabaseConnectionTesterHook.class)
+            .stream()
+            .filter(tester -> tester.consumes(datasource, dbHelper))
+            .findAny();
+
+      if (takesOver.isPresent())
+         return takesOver.get().testConnection(datasource, dbHelper);
+
+      /* create config */
+      DatabaseDatasourceConfig config = (DatabaseDatasourceConfig) datasource.createConfigObject();
+      config.setQuery(dbHelper.createDummyQuery());
+
+      /* build dummy table report */
+      TableReport report = new TableReport();
+      DatasourceContainer datasourceContainer = new DatasourceContainer(datasource, config);
+      report.setDatasourceContainer(datasourceContainer);
+
+      try {
+         simpleDataSupplyer.getData(report);
+      } catch (Exception e) {
+         ConnectionTestFailedException ex = new ConnectionTestFailedException(e.getMessage(), e);
+         ex.setStackTraceAsString(exceptionServices.exceptionToString(e));
+         throw ex;
+      }
+
+      return true;
+   }
 
 }
