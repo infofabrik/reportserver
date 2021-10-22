f82b2df2e39bac5101ae0f82ccea3ab78c250f6d
edu-de <66421684+edu-de@users.noreply.github.com>
2021-10-22 10:03:08 +0200

    refactor

--- src/net/datenwerke/security/service/authenticator/AuthenticatorServiceImpl.java
+++ src/net/datenwerke/security/service/authenticator/AuthenticatorServiceImpl.java
@@ -1,8 +1,9 @@
 package net.datenwerke.security.service.authenticator;
 
+import static java.util.stream.Collectors.toSet;
+
 import java.util.GregorianCalendar;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -35,239 +36,238 @@ import net.datenwerke.security.service.usermanager.entities.User;
 @Singleton
 public class AuthenticatorServiceImpl implements AuthenticatorService {
 
-	protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
-	
-	private final Provider<Set<ReportServerPAM>> pams;
-	private final Provider<RequestUserCache> requestUserCacheProvider;
-	private final Provider<UserManagerService> userManagerServiceProvider;
-	private final HookHandlerService hookHandlerService;
-	private final Provider<CurrentUser> currentUserProvider;
-	private final ThreadLocal<Long> currentUserInThread;
-	private final Provider<HttpServletRequest> servletRequestProvider;
-	private final EventBus eventBus;
-	private final Provider<ApplicationPropertiesService> propertiesServiceProvider;
-	private final Provider<ReportServerService> reportServerServiceProvider;
-	
-	private final Map<Long,Long> lastRequests = new HashMap<Long, Long>();
-	
-	@Inject
-	public AuthenticatorServiceImpl(
-			Provider<Set<ReportServerPAM>> pams,
-			Provider<RequestUserCache> requestUserCacheProvider, 
-			Provider<UserManagerService> userManagerServiceProvider,
-			HookHandlerService hookHandlerService, 
-			Provider<CurrentUser> currentUserProvider,
-			Provider<HttpServletRequest> servletRequestProvider,
-			EventBus eventBus,
-			Provider<ApplicationPropertiesService> propertiesServiceProvider,
-			Provider<ReportServerService> reportServerServiceProvider
-	) {
-		/* store objects */
-		this.pams = pams;
-		this.requestUserCacheProvider = requestUserCacheProvider;
-		this.userManagerServiceProvider = userManagerServiceProvider;
-		this.hookHandlerService = hookHandlerService;
-		this.currentUserProvider = currentUserProvider;
-		this.servletRequestProvider = servletRequestProvider;
-		this.eventBus = eventBus;
-		this.currentUserInThread = new ThreadLocal<Long>();
-		this.propertiesServiceProvider = propertiesServiceProvider;
-		this.reportServerServiceProvider = reportServerServiceProvider;
-	}
+   protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
 
-	public Set<String> getRequiredClientModules() {
-		Set<String> modules = new HashSet<String>();
-		for(ReportServerPAM pam : pams.get()){
-			String modName = pam.getClientModuleName();
-			if(null != modName)
-				modules.add(modName);
-		}
-		return modules;
-	}
+   private final Provider<Set<ReportServerPAM>> pams;
+   private final Provider<RequestUserCache> requestUserCacheProvider;
+   private final Provider<UserManagerService> userManagerServiceProvider;
+   private final HookHandlerService hookHandlerService;
+   private final Provider<CurrentUser> currentUserProvider;
+   private final ThreadLocal<Long> currentUserInThread;
+   private final Provider<HttpServletRequest> servletRequestProvider;
+   private final EventBus eventBus;
+   private final Provider<ApplicationPropertiesService> propertiesServiceProvider;
+   private final Provider<ReportServerService> reportServerServiceProvider;
 
-    public AuthenticationResult authenticate(AuthToken[] tokens) {
-       for (PreAuthenticateHook hook : hookHandlerService.getHookers(PreAuthenticateHook.class))
-          hook.authenticating(tokens);
+   private final Map<Long, Long> lastRequests = new HashMap<Long, Long>();
 
-       AuthenticationResult authRes = evaluateTokens(tokens);
+   @Inject
+   public AuthenticatorServiceImpl(
+         Provider<Set<ReportServerPAM>> pams,
+         Provider<RequestUserCache> requestUserCacheProvider, 
+         Provider<UserManagerService> userManagerServiceProvider,
+         HookHandlerService hookHandlerService, 
+         Provider<CurrentUser> currentUserProvider,
+         Provider<HttpServletRequest> servletRequestProvider, 
+         EventBus eventBus,
+         Provider<ApplicationPropertiesService> propertiesServiceProvider,
+         Provider<ReportServerService> reportServerServiceProvider
+         ) {
+      /* store objects */
+      this.pams = pams;
+      this.requestUserCacheProvider = requestUserCacheProvider;
+      this.userManagerServiceProvider = userManagerServiceProvider;
+      this.hookHandlerService = hookHandlerService;
+      this.currentUserProvider = currentUserProvider;
+      this.servletRequestProvider = servletRequestProvider;
+      this.eventBus = eventBus;
+      this.currentUserInThread = new ThreadLocal<Long>();
+      this.propertiesServiceProvider = propertiesServiceProvider;
+      this.reportServerServiceProvider = reportServerServiceProvider;
+   }
 
-       // call this after for collecting updated session data
-       for (PostAuthenticateHook hook : hookHandlerService.getHookers(PostAuthenticateHook.class))
-          hook.authenticated(authRes);
+   public Set<String> getRequiredClientModules() {
+      return pams.get()
+         .stream()
+         .map(ReportServerPAM::getClientModuleName)
+         .filter(modName -> null != modName)
+         .collect(toSet());
+   }
 
-       if (authRes.isAllowed() && null != authRes.getUser()) {
+   public AuthenticationResult authenticate(final AuthToken[] tokens) {
+      hookHandlerService.getHookers(PreAuthenticateHook.class)
+         .forEach(hook -> hook.authenticating(tokens));
 
-          // https://wiki.owasp.org/index.php/Testing_for_Session_Fixation_(OTG-SESS-003)
-          if (isSessionRenewalEnabled())
-             servletRequestProvider.get().getSession(false).invalidate();
+      AuthenticationResult authRes = evaluateTokens(tokens);
 
-          setCurrentUserId(authRes.getUser().getId());
+      // call this after for collecting updated session data
+      hookHandlerService.getHookers(PostAuthenticateHook.class)
+         .forEach(hook -> hook.authenticated(authRes));
 
-          eventBus.fireEvent(new LoginEvent("user", getCurrentUserId()));
-       } else {
-          String suser = (null != authRes.getUser()) ? authRes.getUser().getUsername() : "NULL";
-          eventBus.fireEvent(new FailedLoginEvent("user", suser));
-       }
+      if (authRes.isAllowed() && null != authRes.getUser()) {
 
-       // call this after for collecting updated session data
-       collectServerInformation(authRes);
+         // https://wiki.owasp.org/index.php/Testing_for_Session_Fixation_(OTG-SESS-003)
+         if (isSessionRenewalEnabled())
+            servletRequestProvider.get().getSession(false).invalidate();
 
-       return authRes;
-    }
-    
-    private void collectServerInformation(AuthenticationResult authRes) {
-       if (authRes.isAllowed()) {
-          HttpServletRequest httpServletRequest = servletRequestProvider.get();
-          if (null == httpServletRequest)
-             return;
+         setCurrentUserId(authRes.getUser().getId());
 
-          ((ReportServerServiceImpl) reportServerServiceProvider.get()).init(httpServletRequest);
-       }
-    }
-	
-	private boolean isSessionRenewalEnabled(){
-		boolean disable = propertiesServiceProvider.get().getBoolean(AuthenticatorService.PROPERTY_KEY_SESSION_RENEWAL_OVERRIDE_DISABLE, false);
-		return ! disable;
-	}
-	
-	@Override
-	public void setAuthenticated(Long userId) {
-		setCurrentUserId(userId);
-		eventBus.fireEvent(new LoginEvent("user", getCurrentUserId()));
-		try{
-			User user = getCurrentUser();
-			if(null == user)
-				logoff();
-		} catch (Exception e){
-			logger.warn( e.getMessage(), e);
-		}
-	}
-	
-	@Override
-	public void setAuthenticatedInThread(Long userId){
-		if(null != currentUserInThread.get())
-			throw new AuthenticatorRuntimeException("User already logged in");
-		currentUserInThread.set(userId);
-	}
-	
-	@Override
-	public void logoffUserInThread(){
-		if(null == currentUserInThread.get())
-			throw new AuthenticatorRuntimeException("No user logged in");
-		currentUserInThread.set(null);
-	}
+         eventBus.fireEvent(new LoginEvent("user", getCurrentUserId()));
+      } else {
+         String suser = (null != authRes.getUser()) ? authRes.getUser().getUsername() : "NULL";
+         eventBus.fireEvent(new FailedLoginEvent("user", suser));
+      }
 
-	private AuthenticationResult evaluateTokens(AuthToken[] tokens){
-		User tmpUser = null;
-		boolean result = true;
+      // call this after for collecting updated session data
+      collectServerInformation(authRes);
 
-		for(ReportServerPAM pam : pams.get()){
-			AuthenticationResult authRes = pam.authenticate(tokens); 
+      return authRes;
+   }
 
-			if(!authRes.isAllowed())
-				result = false;
+   private void collectServerInformation(AuthenticationResult authRes) {
+      if (authRes.isAllowed()) {
+         HttpServletRequest httpServletRequest = servletRequestProvider.get();
+         if (null == httpServletRequest)
+            return;
 
-			User authUser = authRes.getUser();
-			if(null != authUser){
-				if(null == tmpUser)
-					tmpUser = authUser;
+         ((ReportServerServiceImpl) reportServerServiceProvider.get()).init(httpServletRequest);
+      }
+   }
 
-				if(!tmpUser.equals(authUser))
-					result = false;
-			}
-		}
-		if(null == tmpUser)
-			result = false;
+   private boolean isSessionRenewalEnabled() {
+      boolean disable = propertiesServiceProvider.get()
+            .getBoolean(AuthenticatorService.PROPERTY_KEY_SESSION_RENEWAL_OVERRIDE_DISABLE, false);
+      return !disable;
+   }
 
-		return new AuthenticationResult(result, tmpUser);
-	}
+   @Override
+   public void setAuthenticated(Long userId) {
+      setCurrentUserId(userId);
+      eventBus.fireEvent(new LoginEvent("user", getCurrentUserId()));
+      try {
+         User user = getCurrentUser();
+         if (null == user)
+            logoff();
+      } catch (Exception e) {
+         logger.warn(e.getMessage(), e);
+      }
+   }
 
-	public User getCurrentUser() {
-		if(null != currentUserInThread.get()){
-			Long id = currentUserInThread.get();
-			AbstractUserManagerNode userNode = userManagerServiceProvider.get().getNodeById(id);
-			if(!(userNode instanceof User))
-				throw new AuthenticatorRuntimeException("Something went terribly wrong. UserId (" + id + ") does not point at user."); //$NON-NLS-1$ //$NON-NLS-2$
+   @Override
+   public void setAuthenticatedInThread(Long userId) {
+      if (null != currentUserInThread.get())
+         throw new AuthenticatorRuntimeException("User already logged in");
+      currentUserInThread.set(userId);
+   }
 
-			return (User) userNode;	
-		}
-		
-		if(null == getCurrentUserId())
-			throw new AuthenticatorRuntimeException("no user logged in");
+   @Override
+   public void logoffUserInThread() {
+      if (null == currentUserInThread.get())
+         throw new AuthenticatorRuntimeException("No user logged in");
+      currentUserInThread.set(null);
+   }
 
-		return requestUserCacheProvider.get().getUser(getCurrentUserId());
-	}
+   private AuthenticationResult evaluateTokens(AuthToken[] tokens) {
+      User tmpUser = null;
+      boolean result = true;
 
-	public boolean isAuthenticated() {
-		if(null != currentUserInThread.get()){
-			return true;
-		}else if(null != getCurrentUserId()){
-			return true;
-		}else{
-			// try login without any tokens to perform autologin
-			authenticate(new AuthToken[0]);
-			return null != getCurrentUserId();
-		}
-	}
+      for (ReportServerPAM pam : pams.get()) {
+         AuthenticationResult authRes = pam.authenticate(tokens);
 
-	public void logoff() {
-		CurrentUser currentUser = currentUserProvider.get();
-		if(null != currentUser.getSudoParent()){
-			eventBus.fireEvent(new SuEvent("sued_user_end", currentUser.getCurrentUserId()));
-			currentUser.setCurrentUserId(currentUser.getSudoParent());
-			currentUserInThread.set(null);
-			currentUser.setSudoParent(null);
-		}else{
-			eventBus.fireEvent(new LogoutEvent("user", getCurrentUserId()));
-			setCurrentUserId(null);
-			currentUserInThread.set(null);
-			servletRequestProvider.get().getSession().invalidate();
-			try{
-				servletRequestProvider.get().logout();
-			} catch (Exception e){
-				logger.warn( e.getMessage(), e);
-			}
-		}
-	}
-	
-	@Override
-	public void su(User user){
-		Long parent = getCurrentUserId();
-		if(null == parent)
-			throw new AuthenticatorRuntimeException("no user logged in");
+         if (!authRes.isAllowed())
+            result = false;
 
-		eventBus.fireEvent(new SuEvent("sued_user", user.getId()));
-		
-		CurrentUser currentUser = currentUserProvider.get();
-		currentUser.setCurrentUserId(user.getId());
-		currentUser.setSudoParent(parent);
-	}
+         User authUser = authRes.getUser();
+         if (null != authUser) {
+            if (null == tmpUser)
+               tmpUser = authUser;
 
+            if (!tmpUser.equals(authUser))
+               result = false;
+         }
+      }
+      if (null == tmpUser)
+         result = false;
 
-	private Long getCurrentUserId(){
-		try{
-			Long id = currentUserProvider.get().getCurrentUserId();
-			
-			if(null != id)
-				lastRequests.put(id, new GregorianCalendar().getTimeInMillis());
-			
-			return id;
-		}catch(Exception e){
-			return null;
-		}
-	}
-	
-	private void setCurrentUserId(Long userId){
-		try{
-			currentUserProvider.get().setCurrentUserId(userId);
-		}catch(Exception e){
-			logger.warn( e.getMessage(), e);
-		}
-	}
-	
-	@Override
-	public Map<Long, Long> getLastRequests(){
-		return new HashMap<Long, Long>(lastRequests);
-	}
+      return new AuthenticationResult(result, tmpUser);
+   }
+
+   public User getCurrentUser() {
+      if (null != currentUserInThread.get()) {
+         Long id = currentUserInThread.get();
+         AbstractUserManagerNode userNode = userManagerServiceProvider.get().getNodeById(id);
+         if (!(userNode instanceof User))
+            throw new AuthenticatorRuntimeException(
+                  "Something went terribly wrong. UserId (" + id + ") does not point at user."); //$NON-NLS-1$ //$NON-NLS-2$
+
+         return (User) userNode;
+      }
+
+      if (null == getCurrentUserId())
+         throw new AuthenticatorRuntimeException("no user logged in");
+
+      return requestUserCacheProvider.get().getUser(getCurrentUserId());
+   }
+
+   public boolean isAuthenticated() {
+      if (null != currentUserInThread.get()) {
+         return true;
+      } else if (null != getCurrentUserId()) {
+         return true;
+      } else {
+         // try login without any tokens to perform autologin
+         authenticate(new AuthToken[0]);
+         return null != getCurrentUserId();
+      }
+   }
+
+   public void logoff() {
+      CurrentUser currentUser = currentUserProvider.get();
+      if (null != currentUser.getSudoParent()) {
+         eventBus.fireEvent(new SuEvent("sued_user_end", currentUser.getCurrentUserId()));
+         currentUser.setCurrentUserId(currentUser.getSudoParent());
+         currentUserInThread.set(null);
+         currentUser.setSudoParent(null);
+      } else {
+         eventBus.fireEvent(new LogoutEvent("user", getCurrentUserId()));
+         setCurrentUserId(null);
+         currentUserInThread.set(null);
+         servletRequestProvider.get().getSession().invalidate();
+         try {
+            servletRequestProvider.get().logout();
+         } catch (Exception e) {
+            logger.warn(e.getMessage(), e);
+         }
+      }
+   }
+
+   @Override
+   public void su(User user) {
+      Long parent = getCurrentUserId();
+      if (null == parent)
+         throw new AuthenticatorRuntimeException("no user logged in");
+
+      eventBus.fireEvent(new SuEvent("sued_user", user.getId()));
+
+      CurrentUser currentUser = currentUserProvider.get();
+      currentUser.setCurrentUserId(user.getId());
+      currentUser.setSudoParent(parent);
+   }
+
+   private Long getCurrentUserId() {
+      try {
+         Long id = currentUserProvider.get().getCurrentUserId();
+
+         if (null != id)
+            lastRequests.put(id, new GregorianCalendar().getTimeInMillis());
+
+         return id;
+      } catch (Exception e) {
+         return null;
+      }
+   }
+
+   private void setCurrentUserId(Long userId) {
+      try {
+         currentUserProvider.get().setCurrentUserId(userId);
+      } catch (Exception e) {
+         logger.warn(e.getMessage(), e);
+      }
+   }
+
+   @Override
+   public Map<Long, Long> getLastRequests() {
+      return new HashMap<Long, Long>(lastRequests);
+   }
 
 }
