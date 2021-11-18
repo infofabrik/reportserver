2f5a9752c68a9316e18ff53c16dd9c6c858b91fe
edu-de <66421684+edu-de@users.noreply.github.com>
2021-11-18 15:13:34 +0100

    RS-5196: DB Deadlock when creating many objects sequentially in MSSQL

--- src/net/datenwerke/rs/utils/hibernate/SQLServer2008Dialect.java
+++ src/net/datenwerke/rs/utils/hibernate/SQLServer2008Dialect.java
@@ -1,5 +1,7 @@
 package net.datenwerke.rs.utils.hibernate;
 
+import org.hibernate.LockMode;
+import org.hibernate.LockOptions;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
 import org.hibernate.type.descriptor.sql.LongVarcharTypeDescriptor;
 import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
@@ -30,4 +32,35 @@ public class SQLServer2008Dialect extends org.hibernate.dialect.SQLServer2008Dia
 		}
 		return super.remapSqlTypeDescriptor(sqlTypeDescriptor);
 	}
+	
+    /*
+     * Because of a deadlock (RS-5196) we use the old Hibernate 5.0.3 lock hint
+     * behavior, taken from here:
+     * https://github.com/hibernate/hibernate-orm/blob/5.0.3/hibernate-core/src/main
+     * /java/org/hibernate/dialect/SQLServer2005Dialect.java
+     */
+	@Override
+    public String appendLockHint(LockOptions lockOptions, String tableName) {
+        // NOTE : since SQLServer2005 the nowait hint is supported
+        if ( lockOptions.getLockMode() == LockMode.UPGRADE_NOWAIT ) {
+            return tableName + " with (updlock, rowlock, nowait)";
+        }
+
+        final LockMode mode = lockOptions.getLockMode();
+        final boolean isNoWait = lockOptions.getTimeOut() == LockOptions.NO_WAIT;
+        final String noWaitStr = isNoWait ? ", nowait" : "";
+        switch ( mode ) {
+            case UPGRADE:
+            case PESSIMISTIC_WRITE:
+            case WRITE: {
+                return tableName + " with (updlock, rowlock" + noWaitStr + " )";
+            }
+            case PESSIMISTIC_READ: {
+                return tableName + " with (holdlock, rowlock" + noWaitStr + " )";
+            }
+            default: {
+                return tableName;
+            }
+        }
+    }
 }
