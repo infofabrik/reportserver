eca03afe0d20002eaed614e1ba1fa51c4d62417d
edu-de <66421684+edu-de@users.noreply.github.com>
2021-12-08 10:00:52 +0100

    RS-5236: "mkdir" cmd is throwing NPE when trying to create dir in root

--- src/net/datenwerke/rs/terminal/service/terminal/vfs/commands/VfsCommandMkdir.java
+++ src/net/datenwerke/rs/terminal/service/terminal/vfs/commands/VfsCommandMkdir.java
@@ -2,6 +2,9 @@ package net.datenwerke.rs.terminal.service.terminal.vfs.commands;
 
 import java.util.List;
 
+import com.google.inject.Inject;
+
+import net.datenwerke.rs.fileserver.service.fileserver.vfs.FileServerVfs;
 import net.datenwerke.rs.terminal.service.terminal.TerminalSession;
 import net.datenwerke.rs.terminal.service.terminal.helpers.AutocompleteHelper;
 import net.datenwerke.rs.terminal.service.terminal.helpers.CommandParser;
@@ -19,69 +22,71 @@ import net.datenwerke.security.service.security.SecurityTarget;
 import net.datenwerke.security.service.security.rights.Read;
 import net.datenwerke.security.service.security.rights.Write;
 
-import com.google.inject.Inject;
-
 public class VfsCommandMkdir implements TerminalCommandHook {
-	
-	public static final String BASE_COMMAND = "mkdir";
-	
-	private final SecurityService securityService;
-	
-	@Inject
-	public VfsCommandMkdir(
-		SecurityService securityService
-		){
-		
-		/* store objects */
-		this.securityService = securityService;
-	}
-	
-	@Override
-	public boolean consumes(CommandParser parser, TerminalSession session) {
-		return BASE_COMMAND.equals(parser.getBaseCommand());
-	}
 
-	@CliHelpMessage(
-		messageClass = VfsMessages.class,
-		name = BASE_COMMAND,
-		description = "commandMkdir_description",
-		nonOptArgs = {
-			@NonOptArgument(name="dir", description="commandMkdir_argument")
-		}
-	)
-	@Override
-	public CommandResult execute(CommandParser parser, TerminalSession session) {
-		VirtualFileSystemDeamon vfs = session.getFileSystem();
-		
-		List<String> arguments = parser.getNonOptionArguments();
-		if(arguments.isEmpty())
-			throw new IllegalArgumentException();
-		String pathStr = arguments.get(0);
-		PathHelper path = new PathHelper(pathStr);
+   public static final String BASE_COMMAND = "mkdir";
 
-		try{
-			VFSLocation location = vfs.getLocation(path.getPath());
-	
-			if(location.isVirtualLocation())
-				throw new IllegalArgumentException("location is virtual");
-			
-			Object object = location.getObject();
-			if(object instanceof SecurityTarget)
-				securityService.checkRights((SecurityTarget)object, Read.class, Write.class);
-			else{
-				// what now?
-			}
-			
-			vfs.createFolderIn(location, path.getLastPathway());
-			
-			return new CommandResult();
-		} catch(VFSException e){
-			throw new IllegalArgumentException(e);
-		}
-	}
+   private final SecurityService securityService;
 
-	@Override
-	public void addAutoCompletEntries(AutocompleteHelper autocompleteHelper, TerminalSession session) {
-		autocompleteHelper.autocompleteBaseCommand(BASE_COMMAND);
-	}
+   @Inject
+   public VfsCommandMkdir(SecurityService securityService) {
+
+      /* store objects */
+      this.securityService = securityService;
+   }
+
+   @Override
+   public boolean consumes(CommandParser parser, TerminalSession session) {
+      return BASE_COMMAND.equals(parser.getBaseCommand());
+   }
+
+   @CliHelpMessage(
+         messageClass = VfsMessages.class, 
+         name = BASE_COMMAND, 
+         description = "commandMkdir_description", 
+         nonOptArgs = {
+               @NonOptArgument(
+                     name = "dir", 
+                     description = "commandMkdir_argument"
+                     ) 
+               }
+         )
+   @Override
+   public CommandResult execute(CommandParser parser, TerminalSession session) {
+      VirtualFileSystemDeamon vfs = session.getFileSystem();
+
+      List<String> arguments = parser.getNonOptionArguments();
+      if (arguments.isEmpty())
+         throw new IllegalArgumentException();
+      String pathStr = arguments.get(0);
+      PathHelper path = new PathHelper(pathStr);
+
+      try {
+         VFSLocation location = vfs.getLocation(path.getPath());
+
+         if (location.isVirtualLocation())
+            throw new IllegalArgumentException("location is virtual");
+         
+         if (null == location.getFilesystemManager())
+            throw new IllegalArgumentException("cannot create dir in root");
+
+         Object object = location.getObject();
+         if (object instanceof SecurityTarget)
+            securityService.checkRights((SecurityTarget) object, Read.class, Write.class);
+         else {
+            // what now?
+         }
+
+         vfs.createFolderIn(location, path.getLastPathway());
+
+         return new CommandResult();
+      } catch (VFSException e) {
+         throw new IllegalArgumentException(e);
+      }
+   }
+
+   @Override
+   public void addAutoCompletEntries(AutocompleteHelper autocompleteHelper, TerminalSession session) {
+      autocompleteHelper.autocompleteBaseCommand(BASE_COMMAND);
+   }
 }
