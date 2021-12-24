2627e2f08b056e57a08e478c520e4e93ff870ab0
edu-de <66421684+edu-de@users.noreply.github.com>
2021-12-24 16:32:49 +0100

    RS-5347: Remove deprecated hibernate calls from integrity validator

--- src/net/datenwerke/rs/base/service/reportengines/table/maintenance/TableReportIntegrityValidator.java
+++ src/net/datenwerke/rs/base/service/reportengines/table/maintenance/TableReportIntegrityValidator.java
@@ -6,8 +6,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
-import org.hibernate.SQLQuery;
-import org.hibernate.ejb.HibernateEntityManager;
+import org.hibernate.Session;
 import org.hibernate.query.NativeQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -67,23 +66,22 @@ public class TableReportIntegrityValidator implements MaintenanceTask {
    }
 
    private List<FilterSpec> getOrphanedFilterSpecs() {
-      HibernateEntityManager em = (HibernateEntityManager) entityManagerProvider.get();
-      SQLQuery query = em.getSession()
-            .createSQLQuery("SELECT A.ENTITY_ID FROM RS_FILTER_SPEC A " + "LEFT OUTER JOIN ( "
-                  + "SELECT FILTERS_ID FROM RS_FILTER_BLOCK_2_FILTERS) B "
-                  + "ON B.FILTERS_ID = A.ENTITY_ID WHERE B.FILTERS_ID IS NULL");
-      List list = query.list();
+      Session session = (Session) entityManagerProvider.get();
+      NativeQuery<Object> query = session.createNativeQuery("SELECT A.ENTITY_ID FROM RS_FILTER_SPEC A "
+            + "LEFT OUTER JOIN ( " + "SELECT FILTERS_ID FROM RS_FILTER_BLOCK_2_FILTERS) B "
+            + "ON B.FILTERS_ID = A.ENTITY_ID WHERE B.FILTERS_ID IS NULL", Object.class);
+      List<Object> list = query.list();
 
       List<FilterSpec> specList = new ArrayList<>();
       for (Object id : list) {
          if (null != id) {
             FilterSpec spec = null;
             if (id instanceof BigInteger)
-               spec = em.find(FilterSpec.class, ((BigInteger) id).longValue());
+               spec = session.find(FilterSpec.class, ((BigInteger) id).longValue());
             else if (id instanceof Long)
-               spec = em.find(FilterSpec.class, id);
+               spec = session.find(FilterSpec.class, id);
             else
-               spec = em.find(FilterSpec.class, Long.valueOf(id.toString()));
+               spec = session.find(FilterSpec.class, Long.valueOf(id.toString()));
 
             if (null != spec)
                specList.add(spec);
@@ -94,37 +92,36 @@ public class TableReportIntegrityValidator implements MaintenanceTask {
    }
 
    protected List<FilterBlock> getOrphanedFilterBlocks() {
-      HibernateEntityManager em = (HibernateEntityManager) entityManagerProvider.get();
-      SQLQuery query = em.getSession().createSQLQuery("SELECT A.* FROM RS_FILTER_BLOCK A " + "LEFT OUTER JOIN ( "
+      Session session = (Session) entityManagerProvider.get();
+      NativeQuery<FilterBlock> query = session.createNativeQuery("SELECT A.* FROM RS_FILTER_BLOCK A "
+            + "LEFT OUTER JOIN ( "
             + "SELECT ROOT_BLOCK_ID AS BID FROM RS_PRE_FILTER UNION SELECT CHILD_BLOCKS_ID AS BID FROM RS_FILTER_BLOCK_2_CHILD_BL) B "
-            + "ON B.BID = A.ENTITY_ID WHERE B.BID IS NULL");
-      query.addEntity(FilterBlock.class);
-      List list = query.list();
+            + "ON B.BID = A.ENTITY_ID WHERE B.BID IS NULL", FilterBlock.class);
+      List<FilterBlock> list = query.list();
       return list;
    }
 
    public List<Column> getOrphanedColumns() {
-      HibernateEntityManager em = (HibernateEntityManager) entityManagerProvider.get();
-      SQLQuery query = em.getSession()
-            .createSQLQuery("SELECT A.ENTITY_ID FROM RS_COLUMN A " + "LEFT OUTER JOIN ( "
-                  + "SELECT COLUMNS_ID AS CID FROM RS_TABLE_REPORT_2_COLUMN UNION "
-                  + "SELECT ADDITIONAL_COLUMNS_ID AS CID FROM RS_TABLE_REPORT_2_ADD_COLUMN UNION "
-                  + "SELECT COLUMN_ID  AS CID FROM RS_COLUMN_FILTER UNION "
-                  + "SELECT COLUMNA_ID AS CID FROM RS_BINARY_COLUMN_FILTER UNION "
-                  + "SELECT COLUMNB_ID AS CID FROM RS_BINARY_COLUMN_FILTER ) B "
-                  + "ON B.CID = A.ENTITY_ID WHERE B.CID IS NULL");
-      List list = query.list();
+      Session session = (Session) entityManagerProvider.get();
+      NativeQuery<Object> query = session.createNativeQuery("SELECT A.ENTITY_ID FROM RS_COLUMN A "
+            + "LEFT OUTER JOIN ( " + "SELECT COLUMNS_ID AS CID FROM RS_TABLE_REPORT_2_COLUMN UNION "
+            + "SELECT ADDITIONAL_COLUMNS_ID AS CID FROM RS_TABLE_REPORT_2_ADD_COLUMN UNION "
+            + "SELECT COLUMN_ID  AS CID FROM RS_COLUMN_FILTER UNION "
+            + "SELECT COLUMNA_ID AS CID FROM RS_BINARY_COLUMN_FILTER UNION "
+            + "SELECT COLUMNB_ID AS CID FROM RS_BINARY_COLUMN_FILTER ) B "
+            + "ON B.CID = A.ENTITY_ID WHERE B.CID IS NULL", Object.class);
+      List<Object> list = query.list();
 
-      List<Column> colList = new ArrayList<Column>();
+      List<Column> colList = new ArrayList<>();
       for (Object id : list) {
          if (null != id) {
             Column column = null;
             if (id instanceof BigInteger)
-               column = em.find(Column.class, ((BigInteger) id).longValue());
+               column = session.find(Column.class, ((BigInteger) id).longValue());
             else if (id instanceof Long)
-               column = em.find(Column.class, id);
+               column = session.find(Column.class, id);
             else
-               column = em.find(Column.class, Long.valueOf(id.toString()));
+               column = session.find(Column.class, Long.valueOf(id.toString()));
 
             if (null != column)
                colList.add(column);
@@ -134,23 +131,21 @@ public class TableReportIntegrityValidator implements MaintenanceTask {
    }
 
    public List<Filter> getOrphanedSapFilters() {
-      HibernateEntityManager em = (HibernateEntityManager) entityManagerProvider.get();
-      SQLQuery query = em.getSession()
-            .createSQLQuery("SELECT A.* FROM RS_FILTER A " + "LEFT OUTER JOIN ( "
-                  + "SELECT FILTER_ID FROM RS_COLUMN WHERE NOT FILTER_ID IS NULL ) B "
-                  + "ON B.FILTER_ID = A.ENTITY_ID WHERE B.FILTER_ID IS NULL");
-      query.addEntity(Filter.class);
-      List list = query.list();
+      Session session = (Session) entityManagerProvider.get();
+      NativeQuery<Filter> query = session.createNativeQuery("SELECT A.* FROM RS_FILTER A " + "LEFT OUTER JOIN ( "
+            + "SELECT FILTER_ID FROM RS_COLUMN WHERE NOT FILTER_ID IS NULL ) B "
+            + "ON B.FILTER_ID = A.ENTITY_ID WHERE B.FILTER_ID IS NULL", Filter.class);
+      List<Filter> list = query.list();
       return list;
    }
 
    public List<FilterRange> getOrphanedRanges() {
-      HibernateEntityManager em = (HibernateEntityManager) entityManagerProvider.get();
-      NativeQuery query = em.getSession().createSQLQuery("SELECT A.* FROM RS_FILTER_RANGE A " + "LEFT OUTER JOIN ( "
+      Session session = (Session) entityManagerProvider.get();
+      NativeQuery<FilterRange> query = session.createNativeQuery("SELECT A.* FROM RS_FILTER_RANGE A "
+            + "LEFT OUTER JOIN ( "
             + "SELECT EXCLUDE_RANGES_ID AS RANGE_ID FROM RS_FILTER_2_FILTER_RNG_EXC UNION SELECT INCLUDE_RANGES_ID AS RANGE_ID FROM RS_FILTER_2_FILTER_RNG_INC) B "
-            + "ON B.RANGE_ID = A.ENTITY_ID WHERE B.RANGE_ID IS NULL");
-      query.addEntity(FilterRange.class);
-      List list = query.list();
+            + "ON B.RANGE_ID = A.ENTITY_ID WHERE B.RANGE_ID IS NULL", FilterRange.class);
+      List<FilterRange> list = query.list();
       return list;
    }
 
