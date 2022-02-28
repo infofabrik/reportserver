db0f0371a8f290a18cc76a9446bf2b089a89ebc3
edu.de <66421684+edu-de@users.noreply.github.com>
2022-02-28 11:30:29 +0100

    RS-5485: Add "ldaptest filter" terminal command

--- src/net/datenwerke/rs/ldap/service/ldap/locale/LdapMessages.java
+++ src/net/datenwerke/rs/ldap/service/ldap/locale/LdapMessages.java
@@ -8,4 +8,8 @@ public interface LdapMessages extends Messages {
    public final static LdapMessages INSTANCE = LocalizationServiceImpl.getMessages(LdapMessages.class);
 
    String commandLdapimport_description();
+   
+   String commandLdaptest_description();
+   
+   String commandLdaptest_sub_filter_description();
 }
