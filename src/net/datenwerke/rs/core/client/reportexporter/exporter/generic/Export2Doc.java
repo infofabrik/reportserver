548ce583ef74ae55b8694551f297cf4d2dde19ce
edu.de <66421684+edu-de@users.noreply.github.com>
2022-05-05 13:30:11 +0200

    RS-5816: Printer datasinks: allow PDF export

--- src/net/datenwerke/rs/core/client/reportexporter/exporter/generic/Export2Doc.java
+++ src/net/datenwerke/rs/core/client/reportexporter/exporter/generic/Export2Doc.java
@@ -2,11 +2,9 @@ package net.datenwerke.rs.core.client.reportexporter.exporter.generic;
 
 import com.google.gwt.resources.client.ImageResource;
 
-import net.datenwerke.rs.core.client.datasinkmanager.dto.DatasinkDefinitionDto;
 import net.datenwerke.rs.core.client.reportexporter.exporter.ReportExporterImpl;
 import net.datenwerke.rs.core.client.reportexporter.locale.ReportExporterMessages;
 import net.datenwerke.rs.core.client.reportmanager.dto.reports.ReportDto;
-import net.datenwerke.rs.tabledatasink.client.tabledatasink.dto.TableDatasinkDto;
 import net.datenwerke.rs.theme.client.icon.BaseIcon;
 
 public abstract class Export2Doc extends ReportExporterImpl {
@@ -41,8 +39,4 @@ public abstract class Export2Doc extends ReportExporterImpl {
       return false;
    }
    
-   @Override
-   public boolean supportsDatasink(Class<? extends DatasinkDefinitionDto> datasinkType) {
-      return ! datasinkType.equals(TableDatasinkDto.class);
-   }
 }
