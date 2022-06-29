13340516120d4cbc9037da969ab5ae9ab25e235d
edu.de <66421684+edu-de@users.noreply.github.com>
2022-06-29 22:52:53 +0200

    Version 4.2.0

--- src/net/datenwerke/rs/ReportServerServiceConfig.java
+++ src/net/datenwerke/rs/ReportServerServiceConfig.java
@@ -242,7 +242,7 @@ public class ReportServerServiceConfig extends DwGwtFrameworkBase {
 
    private final Logger logger = LoggerFactory.getLogger(getClass().getName());
 
-   public static final String CODE_VERSION = "2022-06-21-12-12-32";
+   public static final String CODE_VERSION = "2022-06-29-18-43-47";
 
    public static final String ENTERPRISE_MODULE_LOCATION = "net.datenwerke.rsenterprise.main.service.RsEnterpriseModule";
    private static final String ENTERPRISE_MODULE_LOAD_MODULE_METHOD = "getEnterpriseModules";
