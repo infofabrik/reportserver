e66a29dafbf8d5d40e9d71da79737f4dfae68382
edu-de <66421684+edu-de@users.noreply.github.com>
2023-06-15 18:07:10 +0200

    RS-7526: Display error when trying to import transport with existing key

--- src/net/datenwerke/rs/transport/service/transport/eximport/TransportManagerImporter.java
+++ src/net/datenwerke/rs/transport/service/transport/eximport/TransportManagerImporter.java
@@ -3,6 +3,7 @@ package net.datenwerke.rs.transport.service.transport.eximport;
 import com.google.inject.Inject;
 
 import net.datenwerke.rs.transport.service.transport.TransportTreeService;
+import net.datenwerke.rs.transport.service.transport.entities.Transport;
 import net.datenwerke.treedb.ext.service.eximport.TreeNodeImportItemConfig;
 import net.datenwerke.treedb.ext.service.eximport.TreeNodeImporter;
 import net.datenwerke.treedb.service.treedb.AbstractNode;
@@ -40,7 +41,15 @@ public class TransportManagerImporter extends TreeNodeImporter {
    protected void finishUpImportCreateMode(TreeNodeImportItemConfig itemConfig, AbstractNode<?> node) {
       super.finishUpImportCreateMode(itemConfig, node);
 
-
+      /* check key */
+      if (node instanceof Transport) {
+         String key = ((Transport) node).getKey();
+         if (null != key) {
+            Transport transportByKey = transportTreeService.getTransportByKey(key);
+            if (null != transportByKey)
+               throw new IllegalStateException("A transport with key '" + key + "' already exists");
+         }
+      }
    }
 
 }
