82bd40fae20448e06d6430ac674173c5bce4038f
edu-de <66421684+edu-de@users.noreply.github.com>
2023-06-15 17:54:03 +0200

    RS-7525: Importing transports is changing their key

--- src/net/datenwerke/rs/transport/service/transport/eximport/TransportManagerImporter.java
+++ src/net/datenwerke/rs/transport/service/transport/eximport/TransportManagerImporter.java
@@ -2,9 +2,7 @@ package net.datenwerke.rs.transport.service.transport.eximport;
 
 import com.google.inject.Inject;
 
-import net.datenwerke.rs.transport.service.transport.TransportService;
 import net.datenwerke.rs.transport.service.transport.TransportTreeService;
-import net.datenwerke.rs.transport.service.transport.entities.Transport;
 import net.datenwerke.treedb.ext.service.eximport.TreeNodeImportItemConfig;
 import net.datenwerke.treedb.ext.service.eximport.TreeNodeImporter;
 import net.datenwerke.treedb.service.treedb.AbstractNode;
@@ -15,15 +13,12 @@ public class TransportManagerImporter extends TreeNodeImporter {
    public static final String IMPORTER_ID = "TransportManagerImporter";
 
    private final TransportTreeService transportTreeService;
-   private final TransportService transportService;
 
    @Inject
    public TransportManagerImporter(
-         TransportTreeService transportTreeService,
-         TransportService transportService
+         TransportTreeService transportTreeService
          ) {
       this.transportTreeService = transportTreeService;
-      this.transportService = transportService;
    }
 
    @Override
@@ -45,9 +40,6 @@ public class TransportManagerImporter extends TreeNodeImporter {
    protected void finishUpImportCreateMode(TreeNodeImportItemConfig itemConfig, AbstractNode<?> node) {
       super.finishUpImportCreateMode(itemConfig, node);
 
-      /* recalculate key */
-      if (node instanceof Transport)
-         transportService.setInitialProperties((Transport) node, transportService.createInitialProperties());
 
    }
 
